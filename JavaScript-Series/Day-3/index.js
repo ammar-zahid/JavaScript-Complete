a2ce8a53f2865d3df680f4e62ace5b89088cc3c9
Ammar Zahid <ammarzahid304@gmail.com>
2023-05-21 21:08:41 +0500

    var, let and const in JS

---
+++ JavaScript-Series/Day-3/index.js
@@ -0,0 +1,13 @@
+console.log("Difference between let , var and const");
+
+var a = 'sigma';
+var a = 'figma';
+console.log(a);
+
+let b = 'Style';
+b = 'Styling';
+console.log(b);
+
+const c = 'Coding';
+// c = 'code';
+console.log(c);
