461492dd42133cba64fe0472058a38ae5cd848ee
Ammar Zahid <ammarzahid304@gmail.com>
2023-05-21 20:55:17 +0500

    Variables in JavaScript

---
+++ JavaScript-Series/Day-2/index.js
@@ -0,0 +1,11 @@
+console.log("I am learning Variables in Javascript");
+
+let name = "Ammar";
+let age = 19;
+let percentage = 89.52;
+let grade = 'A+';
+
+console.log(name);
+console.log(age);
+console.log(percentage);
+console.log(grade);
