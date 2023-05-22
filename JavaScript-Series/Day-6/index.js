aebeb4c7849607ca593eeaedf681c43c5188e1d8
Ammar Zahid <ammarzahid304@gmail.com>
2023-05-22 22:51:19 +0500

    Operators and Expressions

---
+++ JavaScript-Series/Day-6/index.js
@@ -0,0 +1,55 @@
+// Arithmetic Operators
+
+let x = 10;
+let y = 4;
+console.log('x = ', x);
+console.log('y = ', y);
+
+console.log('x + y = ', (x + y) );
+console.log('x - y = ', (x - y) );
+console.log('x x y = ', (x * y) );
+console.log('x / y = ', (x / y) );
+console.log('x % y = ', (x % y) );
+console.log('x ^ y = ', (x ** y) );
+// console.log('x++ = ', (x++));
+// console.log('x = ', x);
+// console.log('x-- = ', (x--));
+// console.log('x = ', x);
+console.log('--x = ', (--x));
+console.log('x = ', x);
+console.log('++x = ', (++x));
+console.log('x = ', x);
+
+// Assignment Operators
+
+let a = 5;
+
+console.log('a+=6 = ', a+=6);
+console.log('a-=6 = ', a-=6);
+console.log('a*=6 = ', a*=6);
+console.log('a/=6 = ', a/=6);
+console.log('a%=6 = ', a%=6);
+console.log('a**=6 = ', a**=6);
+
+// Comparison Operators
+
+let comp1 = 6;
+let comp2 = '6';
+
+console.log('comp1 == comp2 is ', comp1==comp2);
+console.log('comp1 != comp2 is ', comp1!=comp2);
+console.log('comp1 === comp2 is ', comp1===comp2);
+console.log('comp1 !== comp2 is ', comp1!==comp2);
+console.log('comp1 > comp2 is ', comp1>comp2);
+console.log('comp1 < comp2 is ', comp1<comp2);
+console.log('comp1 >= comp2 is ', comp1>=comp2);
+console.log('comp1 <= comp2 is ', comp1<=comp2);
+
+// Logical Operators
+
+let num1 = 8;
+let num2 = 3;
+
+console.log('num1 > num2 && num2 > 90', num1 > num2 && num2 > 90);
+console.log('num1 > num2 || num2 < 90', num1 > num2 && num2 < 90);
+console.log('!(num1 == num2)', !(num1 == num2));
