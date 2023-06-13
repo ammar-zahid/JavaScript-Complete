c4f26939986a3fdd193e8a9654bf325217524dbf
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-13 22:32:44 +0500

    Template Strings

---
+++ JavaScript-Mastery/Part-1/Template-String.js
@@ -0,0 +1,10 @@
+let name = "Ammar";
+let age = 19;
+
+// My name is Ammar and my age is 19
+
+let info = `My name is ${name} and my age is ${age}`;
+console.log(info);
+
+info = "My name is " + name + " and my age is " + age;
+console.log(info);
