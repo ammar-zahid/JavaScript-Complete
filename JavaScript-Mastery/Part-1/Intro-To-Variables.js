fbb1bb4af6f4dab521eac659e222b145ceb32c4d
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-13 21:04:28 +0500

    Variables

---
+++ JavaScript-Mastery/Part-1/Intro-To-Variables.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var firstName = "Captain";
+console.log(firstName);
+
+firstName = "kaptaan";
+console.log(firstName);
+
+var secondName = "Artistic";
+console.log(secondName);
+
+var age = 19;
+console.log(age);
+
+var percentage = 64.5;
+console.log(percentage);
