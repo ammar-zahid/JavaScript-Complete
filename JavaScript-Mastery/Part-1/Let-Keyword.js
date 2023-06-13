a54b264570366954ffdfd1962cfb7fb7768a4e1d
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-13 21:27:53 +0500

    Let Keyword

---
+++ JavaScript-Mastery/Part-1/Let-Keyword.js
@@ -0,0 +1,23 @@
+// use let keyword for declaring variables
+// dont use var for declaring variables
+
+//(block scope vs function scope)
+
+
+// if we use var keyword, we can declare variable of same name more than once, which is not a good thing. We can also change the value of that variable
+
+// var name = "Captain";
+// console.log(name);
+// var name = "Artistic";
+// console.log(name);
+// name = "Pakistan";
+// console.log(name);
+
+
+// if we use let keyword, we cannot declare variable of same name more than once, which is a good thing. We can change the value of that variable.
+
+let name = "Captain";
+console.log(name);
+name = "Artistic";
+console.log(name);
+
