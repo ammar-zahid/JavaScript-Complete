9656e7d7d472b6c46d9948d8dd233967912328f4
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-13 21:39:39 +0500

    String Indexing

---
+++ JavaScript-Mastery/Part-1/String-Indexing.js
@@ -0,0 +1,9 @@
+// C a p t a i n
+// 0 1 2 3 4 5 6
+
+let name = "Captain"; // spaces will aslo be count
+
+console.log(name);
+console.log(name[2]);
+console.log(name.length);
+console.log(name[name.length-1]);
