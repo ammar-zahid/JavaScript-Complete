d6df8c9f3b35ac76b71fd5ba205bc84f02c0b4a7
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-13 22:26:12 +0500

    String Concatenation

---
+++ JavaScript-Mastery/Part-1/String-Concatenation.js
@@ -0,0 +1,11 @@
+let firstName = "Captain";
+let lastName = "Artistic";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+let num1 = "10";
+let num2 = "12";
+// let add = num1 +  num2;
+let add = +num1 +  +num2;
+console.log(add);
