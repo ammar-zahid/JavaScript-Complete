047253ad5c36506c0b3f0476712042f3d289574e
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-13 21:56:05 +0500

    Useful string methods

---
+++ JavaScript-Mastery/Part-1/Useful-String-Methods.js
@@ -0,0 +1,30 @@
+// trim()
+
+let name = "   Captain   ";
+console.log(name);
+console.log(name.length);
+
+let trimmedName = name.trim();
+console.log(trimmedName);
+console.log(trimmedName.length);
+
+// toUpperCase()
+
+let name2 = "artistic";
+console.log(name2);
+let capitalLetters = name2.toUpperCase();
+console.log(capitalLetters);
+
+// toLowerCase()
+
+let name3 = "CAPTAIN";
+console.log(name3);
+name3 = name3.toLowerCase();
+console.log(name3);
+
+// slice()
+
+let city = "Karachi";
+console.log(city);
+city = city.slice(2,6);
+console.log(city);
