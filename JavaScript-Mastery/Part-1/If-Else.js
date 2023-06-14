4df772c68af415844fdfe6ed56f9a153cd384f30
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-14 10:55:14 +0500

    If Else Statements

---
+++ JavaScript-Mastery/Part-1/If-Else.js
@@ -0,0 +1,18 @@
+let age = 17;
+
+if (age >= 18) {
+    console.log('You are eligible for license');
+}
+else {
+    console.log('You are not eligible for license');
+}
+
+
+let num = 22;
+
+if (num % 2 === 0) {
+    console.log("Even");
+}
+else {
+    console.log("Odd");
+}
