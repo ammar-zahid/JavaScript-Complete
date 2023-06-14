67afa41cb2a00f181a5e32b9a6c5c831751418bb
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-14 11:18:17 +0500

    And Or Operators

---
+++ JavaScript-Mastery/Part-1/And-Or-Operator.js
@@ -0,0 +1,15 @@
+let age = 19;
+let name = 'Ammar';
+
+if (age === 19 && name === 'Ammar') {
+    console.log('You are Ammar Zahid');
+} else {
+    console.log('Not you');
+}
+
+if (age === 31 || name[0] === 'A') {
+    console.log('Tu bhi Ammar hai lekin teri age 19 saal nhi hai');
+}
+else {
+    console.log('Tu koi or hai');
+}
