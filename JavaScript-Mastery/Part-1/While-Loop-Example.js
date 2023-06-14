032d8d8cd534229d81470e7c9a1a15fedbf6e0ea
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-14 12:30:49 +0500

    While loop example

---
+++ JavaScript-Mastery/Part-1/While-Loop-Example.js
@@ -0,0 +1,12 @@
+// calculate the sum of first 10 natural numbers
+
+let num = 10;
+let total = 0;
+let i = 1;
+
+while (i <= num) {
+    total = total + i;
+    i++;
+}
+
+console.log(`Sum of first 10 numbers is ${total}`);
