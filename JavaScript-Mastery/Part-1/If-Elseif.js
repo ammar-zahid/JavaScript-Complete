3020f445b90639dcd717402d07164721a85796ce
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-14 11:33:34 +0500

    If elseif Statements

---
+++ JavaScript-Mastery/Part-1/If-Elseif.js
@@ -0,0 +1,23 @@
+let weekDay = 4;
+
+if (weekDay === 1) {
+    console.log('Monday');
+}
+else if (weekDay === 2) {
+    console.log('Tuesday');
+}
+else if (weekDay === 3) {
+    console.log('Wednesday');
+}
+else if (weekDay === 4) {
+    console.log('Thursday');
+}
+else if (weekDay === 5) {
+    console.log('Friday');
+}
+else if (weekDay === 6) {
+    console.log('Saturday');
+}
+else if (weekDay === 7) {
+    console.log('Sunday');
+}
