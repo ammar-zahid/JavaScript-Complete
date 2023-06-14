fdbb1ce83be8f4a7f449d8e936affd028f5e41b1
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-14 11:02:06 +0500

    Truthy and Falsy Values

---
+++ JavaScript-Mastery/Part-1/Truthy-Falsy-Values.js
@@ -0,0 +1,14 @@
+// falsy values
+// false
+// null
+// ""
+// undefined
+// 0
+
+let value = 'Captain';
+
+if (value) {
+    console.log('Truthy Value');
+} else {
+    console.log('Falsy Value');
+}
