b7f41227437054997028d9b90233ac2babe2043b
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-15 11:11:52 +0500

    push, pop, shift, unshift

---
+++ JavaScript-Mastery/Part-1/Push-Pop-Shift-Unshift.js
@@ -0,0 +1,21 @@
+let fruits = ["banana", "apple", "mango"];
+console.log(fruits);
+console.log(fruits.length);
+
+// push
+fruits.push("cherry");
+console.log(fruits);
+
+// pop
+let poppedFruit = fruits.pop();
+console.log(poppedFruit);
+console.log(fruits);
+
+// shift
+let removedFruit = fruits.shift();
+console.log(removedFruit);
+console.log(fruits);
+
+// unshift
+fruits.unshift("grapes");
+console.log(fruits);
