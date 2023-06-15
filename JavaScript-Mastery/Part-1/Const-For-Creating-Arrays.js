b60d4f1f11283af521063536967b24343072e934
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-15 12:37:14 +0500

    Use const for creating arrays

---
+++ JavaScript-Mastery/Part-1/Const-For-Creating-Arrays.js
@@ -0,0 +1,19 @@
+const array1 = ["apple", "mango", "orange"];
+console.log(array1);
+
+// array1 = ["banana", "cherry"];
+// console.log(array1);
+
+array1.push("banana", "cherry");
+console.log(array1);
+
+array1.pop();
+console.log(array1);
+
+array1.shift();
+console.log(array1);
+
+array1.unshift("Grapes");
+console.log(array1);
+
+// we cannot change the elements of constant array but , we can add and subtract items in it by using array methods.
