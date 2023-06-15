189c958a2d6e3426ba509f728dc40532213b7fec
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-15 12:09:55 +0500

    ways to clone array

--- JavaScript-Mastery/Part-1/Clone-Concat-Spread-Array.js
+++ JavaScript-Mastery/Part-1/Clone-Concat-Spread-Array.js
@@ -6,7 +6,17 @@ let array1 = ["item1", "item2"];
 
 // let array2 = array1.slice(0);
 // let array2 = [].concat(array1);
-let array2 = [...array1];
+// let array2 = [...array1];
+
+// let array2 = array1.slice(0).concat(["item4", "item5"]);
+// let array2 = [].concat(array1, ["item4", "item5", "item6"]);
+// let array2 = [...array1, "item6", "item7"];
+
+let anotherArray = ["box1", "box2"];
+
+// let array2 = array1.slice(0).concat(anotherArray);
+// let array2 = array1.concat(anotherArray);
+let array2 = [...array1, ...anotherArray];
 
 array1.push("item3");
 
