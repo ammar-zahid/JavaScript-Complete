b021bf45bdeddf9757c810180d0b6efa8368ac75
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-16 15:18:35 +0500

    Intro to objects

---
+++ JavaScript-Mastery/Part-1/Intro-To-Objects.js
@@ -0,0 +1,18 @@
+// object stores object literals and key value pairs
+
+const info = {
+    name: "Ammar",
+    age: 19,
+    gender: "male",
+    phone: 485749,
+    hobbies : ["singing", "programming", "eating"]
+}
+
+console.log(info);
+console.log(typeof info);
+
+console.log(info.age);
+console.log(info.hobbies);
+info.job = "designer";
+
+console.log(info);
