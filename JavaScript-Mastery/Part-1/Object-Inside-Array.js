7dd8b010677fe84e247844e2e97411a495df1fd5
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-16 22:09:31 +0500

    Object Inside Array

---
+++ JavaScript-Mastery/Part-1/Object-Inside-Array.js
@@ -0,0 +1,11 @@
+let usersInformation = [
+    {userId: 342, userName: "ammar", userGender: "male"},
+    {userId: 457, userName: "zahid", userGender: "male"},
+    {userId: 378, userName: "aslam", userGender: "male"},
+]
+
+console.log(usersInformation);
+
+for (let userInfo of usersInformation) {
+    console.log(userInfo);
+}
