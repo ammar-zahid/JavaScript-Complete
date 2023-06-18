dad8a92a8ecc457f99a326dafd080a27a8da9107
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-18 14:05:13 +0500

    Block Scope vs Function Scope

---
+++ JavaScript-Mastery/Part-1/Block-Vs-Function-Scope.js
@@ -0,0 +1,30 @@
+{
+    let value1 = 34;
+    console.log(value1);
+}
+// console.log(value1);
+
+{
+    const value2 = 94;
+    console.log(value2);   
+}
+// console.log(value2);
+
+{
+    var value3 = 89;
+    console.log(value3);
+}
+console.log(value3);
+
+const app = () => {
+    let firstName = "Ammar";
+    function name() {
+        let secondName = "Zahid";
+        // console.log(firstName, secondName);
+    }
+    console.log(firstName, secondName);
+    name();
+}
+
+app();
+
