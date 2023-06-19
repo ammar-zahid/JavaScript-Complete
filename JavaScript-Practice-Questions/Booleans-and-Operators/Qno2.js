d0963becb965d79fc377c60feb5b540becec6f40
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-19 21:07:47 +0500

    Added Q2

---
+++ JavaScript-Practice-Questions/Booleans-and-Operators/Qno2.js
@@ -0,0 +1,12 @@
+// Comparison Result: Write a function that takes two numbers as input and compares them using the greater than (>) operator. Return true if the first number is greater than the second, false otherwise.
+
+function compare(x , y) {
+    if (x > y) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(compare(9,7))
