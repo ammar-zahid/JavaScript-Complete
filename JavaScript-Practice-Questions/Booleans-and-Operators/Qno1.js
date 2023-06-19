07ae80e758618226b005ec65a67bd6623fab2455
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-19 21:05:09 +0500

    Added Q1

---
+++ JavaScript-Practice-Questions/Booleans-and-Operators/Qno1.js
@@ -0,0 +1,12 @@
+// Boolean Check: Write a function that takes a value as input and returns true if the value is a boolean, false otherwise.
+
+function checkBoolean(value){
+    if (value == true) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(checkBoolean(true));
