2df12267aaa9508ae3f7a79e8c021e7dcdeb7afa
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-19 15:57:44 +0500

    String Indexing Question

---
+++ JavaScript-Practice-Questions/String-Indexing/Qno1.js
@@ -0,0 +1,10 @@
+// Write a JavaScript function that takes a string as input and returns the character at the specified index.
+// Example:
+// Input: stringIndex('Hello', 2)
+// Output: 'l'
+
+const stringIndex = (str, index) => {
+    return str[index];
+}
+
+console.log(stringIndex("Captain", 3));
