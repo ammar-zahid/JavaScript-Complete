2902af9e9684c9ad6dca6fb132ffda433aa42a76
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-19 17:45:34 +0500

    Added Q8

---
+++ JavaScript-Practice-Questions/Strings/Qno8.js
@@ -0,0 +1,20 @@
+// Useful Strings methods
+
+let str = "javascript";
+console.log(str.length);
+console.log(str.toUpperCase());
+
+let str1 = "JAVASCRIPT";
+console.log(str1.toLowerCase());
+console.log(str.charAt(3));
+
+console.log(str.indexOf('s'));
+console.log(str.slice(3,7))
+console.log(str.substring(1,7))
+
+let string = "Hello World";
+console.log(string.split(' '));
+
+console.log(string.trim());
+console.log(string.startsWith("Hello"));
+console.log(string.endsWith("World"))
