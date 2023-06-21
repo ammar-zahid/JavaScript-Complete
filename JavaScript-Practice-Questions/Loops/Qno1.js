fa1450daae5cc04cf2a0cacb5465930a255f0c0e
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-21 21:11:09 +0500

    Added Q1

---
+++ JavaScript-Practice-Questions/Loops/Qno1.js
@@ -0,0 +1,16 @@
+// Write a JavaScript program to print all the numbers from 1 to 100. For multiples of 3, print "Fizz" instead of the number, and for multiples of 5, print "Buzz" instead of the number. For numbers that are multiples of both 3 and 5, print "FizzBuzz".
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 !== 0){
+        console.log("Fizz");
+    }
+    else if (i % 5 === 0 && i % 3 !== 0) {
+        console.log("Buzz");
+    }
+    else if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    }
+    else {
+        console.log(i);
+    }
+}
