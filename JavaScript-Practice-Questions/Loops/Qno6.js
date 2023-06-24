6ae240fb1de446370cdffacfb4a216b6bacb8eb1
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-24 21:10:49 +0500

    Added Q6

---
+++ JavaScript-Practice-Questions/Loops/Qno6.js
@@ -0,0 +1,9 @@
+// Write a JavaScript program to print the multiplication table of a given number using a do-while loop.
+
+let num = 4;
+let i = 0;
+
+do {
+    console.log(`${num} x ${i} = ${num * i}`);
+    i++;
+} while (i <= 10);
