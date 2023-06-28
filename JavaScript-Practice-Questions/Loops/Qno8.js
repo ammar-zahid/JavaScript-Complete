cf6bfb5bd7c77bb9c6020fc01baa78c8aa771aca
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-28 20:08:48 +0500

    Added Q8

---
+++ JavaScript-Practice-Questions/Loops/Qno8.js
@@ -0,0 +1,7 @@
+// Write a program to iterate over an array of numbers using the for...of loop and print each number to the console.
+
+const array = [1,2,5,6,8,9];
+
+for (const number of array) {
+    console.log(number);
+}
