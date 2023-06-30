4b01675b32289be2569f0a835dfb03d2b8b24500
Ammar Zahid <ammarzahid304@gmail.com>
2023-06-30 21:28:33 +0500

    Added Q12

---
+++ JavaScript-Practice-Questions/Loops/Qno12.js
@@ -0,0 +1,14 @@
+// Write a function that takes an array of numbers and uses the for...of loop to find the sum of all the numbers. Return the sum.
+
+
+let sum = 0;
+const sumOfNumbers = (arr) => {
+    for (number of arr) {
+        sum = sum + number;
+    }
+
+    return sum;
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(sumOfNumbers(arr));
