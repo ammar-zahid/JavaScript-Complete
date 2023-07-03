9eaf151a2efa1290af4c7ccff52c42f2ec3eb7e3
Ammar Zahid <ammarzahid304@gmail.com>
2023-07-03 16:36:52 +0500

    Added Q11

---
+++ JavaScript-Practice-Questions/Arrays/Qno11.js
@@ -0,0 +1,12 @@
+// Swap Variables: Write a function that takes two numbers as input and swaps their values using array destructuring.
+
+function swap (num1, num2) {
+    let x = num2;
+    let y = num1;
+
+    console.log(`num1 is ${x}\nnum2 is ${y}`);
+}
+
+let num1 = 5;
+let num2 = 8;
+swap(num1, num2)
