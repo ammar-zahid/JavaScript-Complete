838ebbc29612592f579f2f8ef6ba9010661225f2
Ammar Zahid <ammarzahid304@gmail.com>
2023-07-18 21:14:27 +0500

    Every Method

---
+++ JavaScript-Mastery/Part-1/Every-Method.js
@@ -0,0 +1,10 @@
+const users = [
+    {userID: 1, userName: 'Ammar', userAge: 12},
+    {userID: 2, userName: 'Fernandes', userAge: 20},
+    {userID: 3, userName: 'Tokyo', userAge: 49},
+    {userID: 4, userName: 'Rio', userAge: 34},
+]
+
+console.log(users.every((age) => {
+    return age.userAge > 18
+}))
