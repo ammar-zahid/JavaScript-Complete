ea1b27a6178fc106e50e703a3033dc143580a8bf
Ammar Zahid <ammarzahid304@gmail.com>
2023-07-19 20:23:14 +0500

    sets

---
+++ JavaScript-Mastery/Part-1/Sets.js
@@ -0,0 +1,11 @@
+// set dont accept one value twice or more 
+// it is to store unique values 
+// it has its own methods
+// order of data is not guaranteed
+
+
+let data = new Set([1,2,3,4]);
+data.add('ammar')
+data.add(19)
+data.add(2004)
+console.log(data)
