9346c14bf5694fa7f0dbd79db9c694f364f441df
Ammar Zahid <ammarzahid304@gmail.com>
2023-07-20 23:26:39 +0500

    This keyword

---
+++ JavaScript-Mastery/Part-1/This-Keyword.js
@@ -0,0 +1,4 @@
+// JavaScript “this” keyword. “This” keyword refers to an object that is executing the current piece of code. It references the object that is executing the current function. If the function being referenced is a regular function, “this” references the global object.
+
+console.log(this)
+console.log(window)
