a39551d9a983b3f8f722beac584fc2088a97cb76
Ammar Zahid <131453458+ammar-zahid@users.noreply.github.com>
2023-08-20 21:47:11 +0500

    Added Question

---
+++ Learn-What-Matters/Questions/04.js
@@ -0,0 +1,8 @@
+// write a js function to convert an array elements into string.
+
+function elements (array) {
+    return array.toString();
+}
+
+const answer = elements(["red", "green", "blue", "yellow"])
+console.log(answer)
